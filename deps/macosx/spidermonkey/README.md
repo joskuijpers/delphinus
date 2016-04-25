edc05ada313609cba664c991d10fd5f0ebf962e5
Jos Kuijpers <jkuijpers@jarvix.nl>
2016-04-25 17:52:49 +0200

    Add spidermonkey. Change dependency folder structure.

---
+++ deps/macosx/spidermonkey/README.md
@@ -0,0 +1,39 @@
+Build
+=====
+
+
+Clone the mozilla-central repository.
+
+```
+DEST_PATH = ~/Development/delphinus/deps/macosx/spidermonkey
+
+cd mozilla-central/js/src
+mkdir build_OPT.OBJ
+cd build_OPT.OBJ
+
+# --disable-shared-js
+
+# For debug build
+../configure --disable-tests --enable-llvm-hacks --without-intl-api
+
+# For release build
+../configure --disable-tests --enable-release --disable-debug --enable-strip --enable-install-strip --enable-optimize=-O3 --enable-llvm-hacks --without-intl-api
+
+xcrun make -j4
+
+rm -R $DEST_PATH/include/
+cp -RL dist/include/ $DEST_PATH/include/
+
+cp -RL dist/bin/js $DEST_PATH
+cp -RL dist/bin/libmozglue.dylib $DEST_PATH
+cp -RL dist/sdk/lib/libmozglue.dylib $DEST_PATH
+cp -RL dist/sdk/lib/libmozjs-45.dylib $DEST_PATH
+cp js/src/libjs_static.a $DEST_PATH
+
+cd $DEST_PATH
+
+# Release only
+xcrun strip -S libjs_static.a
+xcrun lipo -info libjs_static.a
+
+```
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,21 @@
 //
 
 #include <iostream>
+#include <memory.h>
+
 #include <SDL2/SDL.h>
+#include "jsapi.hpp"
+
+/* The error reporter callback. */
+void reportError(JSContext *cx, const char *message, JSErrorReport *report)
+{
+    fprintf(stderr, "%s:%u:%s\n",
+            report->filename ? report->filename : "<no filename>",
+            (unsigned int) report->lineno,
+            message);
+}
+
+static JSClass global_class;
 
 int main(int argc, const char * argv[]) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -16,6 +30,70 @@ int main(int argc, const char * argv[]) {
     }
 
     std::cout << "Hello" << std::endl;
+
+    memset(&global_class, 0, sizeof(JSClass));
+    global_class.name = "global";
+    global_class.flags = JSCLASS_GLOBAL_FLAGS;
+    global_class.trace = JS_GlobalObjectTraceHook;
+
+
+    JS_Init();
+
+    JSRuntime *rt = JS_NewRuntime(8L * 1024L * 1024L);
+    if (rt == nullptr)
+        return 1;
+
+    JS_SetErrorReporter(rt, reportError);
+
+    JSContext *cx = JS_NewContext(rt, 8192);
+    if (cx == nullptr)
+        return 1;
+
+    {
+        JSAutoRequest ar(cx); // exit this any time spinning event loop?
+
+        JS::RootedObject global(cx, JS_NewGlobalObject(cx, &global_class, nullptr, JS::OnNewGlobalHookOption::FireOnNewGlobalHook, JS::CompartmentOptions()));
+        if (!global)
+            return 1;
+
+        JS::RootedValue rval(cx);
+
+        {
+            JSAutoCompartment ac(cx, global);
+
+            if (!JS_InitStandardClasses(cx, global))
+                return 1;
+
+            const char *script = "function foo() { return 'hello'+'world, it is '+new Date(); }; foo();";
+            const char *filename = "noname";
+            int lineno = 1;
+
+            JS::OwningCompileOptions options(cx);
+            options.setFileAndLine(cx, filename, lineno);
+            options.setIntroductionScript(nullptr);
+            options.setVersion(JSVERSION_LATEST);
+            options.setUTF8(true);
+            options.asmJSOption = JS::AsmJSOption::Enabled;
+            options.strictOption = true;
+
+            JS::RootedScript jsScript(cx);
+
+            if (!JS::Compile(cx, options, script, strlen(script), &jsScript)) {
+                std::cerr << "Cant compile script" << std::endl;
+                return 1;
+            }
+
+            JS_MaybeGC(cx);
+
+            if (!JS_ExecuteScript(cx, jsScript, &rval)) {
+                std::cerr << "Cant eval script" << std::endl;
+                return 1;
+            }
+        }
+
+        JSString *str = rval.toString();
+        printf("%s\n", JS_EncodeString(cx, str));
+    }
 
     SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
     if (win == nullptr){
@@ -32,24 +110,35 @@ int main(int argc, const char * argv[]) {
         return 1;
     }
 
-    while(true) {
+    bool running = true;
+    unsigned int num = 0;
+
+    while(running) {
+        /// EVENTS START
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
-            std::cout << "Got event" << std::endl;
+            std::cout << "Got event " << num++ << ": " << event.type << std::endl;
+
+            if (event.type == SDL_QUIT) {
+                std::cout << "Quit!" << std::endl;
+                running = false;
+                break;
+            }
         }
+        /// EVENTS END
 
-        //First clear the renderer
+        /// RENDER START
         SDL_RenderClear(ren);
-        //Draw the texture
-//        SDL_RenderCopy(ren, tex, NULL, NULL);
-        //Update the screen
+            /// RENDER USER FUNC
         SDL_RenderPresent(ren);
-
-        //Take a quick break after all that hard work
-        SDL_Delay(1000);
+        /// RENDER END
     }
 
     SDL_Quit();
+
+    JS_DestroyContext(cx);
+    JS_DestroyRuntime(rt);
+    JS_ShutDown();
 
     return 0;
 }
