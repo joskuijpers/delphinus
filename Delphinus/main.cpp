5940d74388af0a4c406d417fffc34e95e5097edb
Jos Kuijpers <jkuijpers@jarvix.nl>
2016-04-25 12:05:09 +0200

    Initial commit.

---
+++ Delphinus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Delphinus
+//
+//  Created by Jos Kuijpers on 25/04/16.
+//  Copyright © 2016 Jarvix. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
